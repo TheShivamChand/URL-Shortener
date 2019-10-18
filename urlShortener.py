c214d76dc42a05ec3992b249adb1f75d71f43d7e
Satyam Chand Deopa <53179013+SatyamChand@users.noreply.github.com>
2019-10-18 14:05:44 +0530

    Added Indentations

--- urlShortener.py
+++ urlShortener.py
@@ -14,14 +14,18 @@ except ImportError:
 
 import sys
 
+
+
 def make_tiny(url):
 	request_url = ('http://tinyurl.com/api-create.php?' + urlencode({'url':url}))
 	with contextlib.closing(urlopen(request_url)) as response:
 		return response.read().decode('utf-8')
 
+	
 def main():
 	for tinyurl in map(make_tiny,sys.argv[1:]):
 		print(tinyurl)
 
+		
 if __name__=='__main__':
 	main()
